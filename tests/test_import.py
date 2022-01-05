7b13b2fc1e9fd359c9ecd460971239162f3d8c0d
enesgencer18 <83446304+enesgencer18@users.noreply.github.com>
2022-01-05 14:18:35 +0300

    Create test_import.py

---
+++ tests/test_import.py
@@ -0,0 +1,12 @@
+print("Import specified packages to check if everything works properly")
+
+import jupyter
+import jupyterlab
+import numpy
+import scipy
+import matplotlib
+import opencv-contrib-python
+import scikit-image
+import scikit-learn
+
+print("Well done!")
